797dc2bec23c26f5024630df8f2d236cc18ba1dc
Michael Patterson <mpatterson@pillartechnology.com>
2017-02-19 21:45:53 -0600

    WIP: Implement eq, ne, ge, gt, and in comparisons.

--- test/test_virtual_machine_comparisons.py
+++ test/test_virtual_machine_comparisons.py
@@ -42,3 +42,93 @@ class TestVirtualMachine:
         self.vm.push_frame(frame)
         self.vm.instr_COMPARE_OP([1])
         assert frame.stack == [True]
+
+    def test_instr_COMPARE_OP__eq_sets_top_of_stack_to_false(self):
+        a = 10
+        b = 7
+        frame = MagicMock()
+        frame.stack = [a, b]
+        self.vm.push_frame(frame)
+        self.vm.instr_COMPARE_OP([2])
+        assert frame.stack == [False]
+
+    def test_instr_COMPARE_OP__eq_sets_top_of_stack_to_true(self):
+        a = 10
+        b = 10
+        frame = MagicMock()
+        frame.stack = [a, b]
+        self.vm.push_frame(frame)
+        self.vm.instr_COMPARE_OP([2])
+        assert frame.stack == [True]
+
+    def test_instr_COMPARE_OP__ne_sets_top_of_stack_to_false(self):
+        a = 10
+        b = 10
+        frame = MagicMock()
+        frame.stack = [a, b]
+        self.vm.push_frame(frame)
+        self.vm.instr_COMPARE_OP([3])
+        assert frame.stack == [False]
+
+    def test_instr_COMPARE_OP__ne_sets_top_of_stack_to_true(self):
+        a = 10
+        b = 7
+        frame = MagicMock()
+        frame.stack = [a, b]
+        self.vm.push_frame(frame)
+        self.vm.instr_COMPARE_OP([3])
+        assert frame.stack == [True]
+
+    def test_instr_COMPARE_OP__gt_sets_top_of_stack_to_false(self):
+        a = 7
+        b = 10
+        frame = MagicMock()
+        frame.stack = [a, b]
+        self.vm.push_frame(frame)
+        self.vm.instr_COMPARE_OP([4])
+        assert frame.stack == [False]
+
+    def test_instr_COMPARE_OP__gt_sets_top_of_stack_to_true(self):
+        a = 10
+        b = 7
+        frame = MagicMock()
+        frame.stack = [a, b]
+        self.vm.push_frame(frame)
+        self.vm.instr_COMPARE_OP([4])
+        assert frame.stack == [True]
+
+    def test_instr_COMPARE_OP__ge_sets_top_of_stack_to_false(self):
+        a = 7
+        b = 10
+        frame = MagicMock()
+        frame.stack = [a, b]
+        self.vm.push_frame(frame)
+        self.vm.instr_COMPARE_OP([5])
+        assert frame.stack == [False]
+
+    def test_instr_COMPARE_OP__ge_sets_top_of_stack_to_true(self):
+        a = 10
+        b = 10
+        frame = MagicMock()
+        frame.stack = [a, b]
+        self.vm.push_frame(frame)
+        self.vm.instr_COMPARE_OP([5])
+        assert frame.stack == [True]
+
+    def test_instr_COMPARE_OP__in_sets_top_of_stack_to_false(self):
+        a = 7
+        b = [10]
+        frame = MagicMock()
+        frame.stack = [a, b]
+        self.vm.push_frame(frame)
+        self.vm.instr_COMPARE_OP([6])
+        assert frame.stack == [False]
+
+    def test_instr_COMPARE_OP__in_sets_top_of_stack_to_true(self):
+        a = 10
+        b = [7, 10]
+        frame = MagicMock()
+        frame.stack = [a, b]
+        self.vm.push_frame(frame)
+        self.vm.instr_COMPARE_OP([6])
+        assert frame.stack == [True]
