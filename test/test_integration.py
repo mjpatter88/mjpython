b4300b794a3ee5122ea4b896bb368ce0c71bc2a3
Michael Patterson <mjpatter88@gmail.com>
2017-02-27 18:37:00 -0600

    Add an integration test for the continue statement.

--- test/test_integration.py
+++ test/test_integration.py
@@ -80,3 +80,15 @@ class TestByteCodeObjectExecution():
                 break
             return x
         assert self.vm.run_code(test_func.__code__) == 11
+
+    def test_while_loop_continue(self):
+        def test_func():
+            x = 10
+            y = 0
+            while y < 5:
+                y = y + 1
+                if True:
+                    continue
+                x += 10
+            return x
+        assert self.vm.run_code(test_func.__code__) == 10
