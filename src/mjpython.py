6c0cff6b8ed72ec619044cc124db2c4365b5b5ca
Michael Patterson <mjpatter88@gmail.com>
2017-03-25 17:11:34 -0500

    Refactor mjpython launcher to make it easier to execute scripts.

--- src/mjpython.py
+++ src/mjpython.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 from virtual_machine import VirtualMachine
 
--- test/acc/test_printing.py
+++ test/acc/test_printing.py
@@ -10,6 +10,6 @@ class TestPrinting:
         test_target = os.path.join(self.test_dir, test_file_name)
 
         output = subprocess.check_output(["python", test_target])
-        my_output = subprocess.check_output(["python", "mjpython.py", test_target])
+        my_output = subprocess.check_output(["python", "src/mjpython.py", test_target])
 
         assert my_output == output
