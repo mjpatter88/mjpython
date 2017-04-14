d4debb8e33ab1062bcd5b73b2ca14f2fb9bcbfa0
Michael Patterson <mjpatter88@gmail.com>
2017-04-13 23:34:01 -0500

    WIP: Start working on the debugger gui design.

--- src/debug/dbg.py
+++ src/debug/dbg.py
@@ -1,7 +1,30 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QListWidget
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QWidget
+
+
+class Content(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        grid = QGridLayout()
+        grid.setSpacing(10)
+
+        l = QListWidget()
+        l.addItem("Test")
+        l.addItem("Test2")
+        grid.addWidget(l, 0, 0)
+
+        l2 = QListWidget()
+        l2.addItem("Test3")
+        l2.addItem("Test4")
+        grid.addWidget(l2, 0, 1)
+
+        self.setLayout(grid)
 
 
 class Dbg(QMainWindow):
@@ -9,6 +32,8 @@ class Dbg(QMainWindow):
         super().__init__()
         self.setWindowTitle("mjpython debugger")
         self.setGeometry(100, 100, 1500, 800)
+
+        self.setCentralWidget(Content())
 
         if not test:
             self.show()
