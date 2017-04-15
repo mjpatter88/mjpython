cd27784f0c1cbc6dc55596848cfcc0e673bc863e
Michael Patterson <mpatterson@pillartechnology.com>
2017-04-14 23:18:37 -0500

    Cleanup dead code and create an easy entry point to the debugger.

--- src/mjpython.py
+++ src/mjpython.py
@@ -4,13 +4,9 @@ import sys
 from virtual_machine import VirtualMachine
 
 file_name = sys.argv[1]
-# print(file_name + " ", end="")
 
 with open(file_name) as script:
     code = compile(script.read(), file_name, 'exec')
 
-# print("compiled")
-# import dis
-# print(dis.dis(code))
 vm = VirtualMachine()
 vm.run_code(code)
